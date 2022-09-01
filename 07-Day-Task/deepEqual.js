118ac02e11e24c19f0704d1e86b4cdffecaeeab4
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2022-09-01 08:56:24 +0400

    equals Update

--- 07-Day-Task/deepEqual.js
+++ 07-Day-Task/deepEqual.js
@@ -1,10 +1,9 @@
 function deepEquals(x, y) {
-  //   console.log(typeof x);
-  //   console.log(JSON.stringify(x).split("").sort());
-  //   console.log(JSON.stringify(x).split("").sort());
-
-  //   if (typeof x == "object" || typeof y == "object") {
-  if (
+  if (x === undefined || y === undefined) {
+    return false;
+  } else if (x === undefined && y === undefined) {
+    return true;
+  } else if (
     JSON.stringify(x).split("").sort().toString() ===
     JSON.stringify(y).split("").sort().toString()
   ) {
@@ -24,21 +23,27 @@ console.log();
 console.log();
 console.log();
 console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
+console.log(deepEquals([[1, true, "abc"], [2]], [[1, true, "abc"]]));
+console.log(
+  deepEquals([[1, [2, 3, [4, 5]]], [2]], [[1, [2, 3, [4, "5"]]], [2]])
+);
+console.log(deepEquals(undefined, undefined));
+console.log(
+  deepEquals([1, 2, [3, 4, { a: "abc" }]], [1, 2, [3, 4, { a: "abc" }]])
+);
+console.log(
+  deepEquals({ a: 123, c: true, b: "abc" }, { a: 123, b: "abc", c: true })
+);
+console.log(deepEquals([[0, [1]]], [[0, [1, 2]]]));
+console.log(deepEquals([[0, [1, 2]]], [[0, [1]]]));
+console.log(deepEquals([[1]], [[]]));
+console.log(deepEquals([[]], []));
+console.log(deepEquals({ a: "123" }, { a: "123" }));
+console.log(deepEquals([], []));
+console.log(deepEquals("a", "b"));
+console.log(deepEquals("", ""));
+console.log(deepEquals(1, 0));
+console.log(deepEquals(1, 1));
 
 console.log(
   deepEquals({ a: 123, c: true, b: "abc" }, { a: 123, b: "abc", c: true })
